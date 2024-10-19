f7e5673b2921ae9829fc70f9b709ad0e3fbcebb6
Kelvin Musselli <kelvin.serva@arquivei.com.br>
2024-10-19 17:23:31 -0300

    feat: add styled component in ssr
    Add configurate to render styled component
    in server side when need.

---
+++ src/shared/styled-registry/globalStyles.ts
@@ -0,0 +1,10 @@
+"use client";
+
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`;
---
+++ src/shared/styled-registry/index.ts
@@ -0,0 +1,1 @@
+export { default as StyledRegistry } from "./styled-registry";
